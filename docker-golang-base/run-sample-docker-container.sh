06c6649ee114f18c35856e4efa7c8cc6bff3c9e5
hellgate75 <hellgate75@gmail.com>
2020-03-14 11:49:15 +0000

    changes to docker run sample for universal base image

--- docker-golang-base/run-sample-docker-container.sh
+++ docker-golang-base/run-sample-docker-container.sh
@@ -3,4 +3,4 @@ DOCKER_VERSION="universal"
 if [ "" != "$1" ]; then
 	DOCKER_VERSION="$1"
 fi
-docker run -it --rm --name golang-universal golang:$DOCKER_VERSION bash
+docker run -it --rm --name golang-$DOCKER_VERSION golang:$DOCKER_VERSION bash
