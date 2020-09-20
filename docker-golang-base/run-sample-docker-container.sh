2d0ae2faba46a702786c3071e549008c45cfa932
hellgate75 <hellgate75@gmail.com>
2020-09-20 16:14:45 +0100

    Adding sample container run scripts features

--- docker-golang-base/run-sample-docker-container.sh
+++ docker-golang-base/run-sample-docker-container.sh
@@ -1,18 +1,23 @@
 #!/bin/sh
 GOVER="$(wget -q -O - https://golang.org/doc/devel/release.html | grep "<h2 id=\"go" | awk 'BEGIN {FS=OFS=" "}{print $2}' | awk 'BEGIN {FS=OFS="\""}{print $2}'|head -1|awk 'BEGIN {FS=OFS="go"}{print $2}')"
 
-# Removing previous containers
-if [ "" != "$(docker ps -a|grep -v IMAGE|grep "golang-$GOVER")" ] {
-	docker rm -f golang-$GOVER
-}
-
 # Checking custom docker image version from command line argument(s)
 DOCKER_VERSION="universal"
 if [ "" != "$1" ]; then
 	DOCKER_VERSION="$1"
+	if [ "universal" != "$1" ]; then
+		GOVER="$1"
+	fi
 fi
 
+# Removing previous containers
+if [ "" != "$(docker ps -a|grep -v IMAGE|grep "golang-$GOVER")" ] then {
+	echo "Removing existing docker container: golang-$GOVER ..."
+	docker rm -f golang-$GOVER
+}
+
 # Running and logging universal container without code
+echo "Running docker container: golang-$GOVER (Go version $GOVER) ..."
 docker run -d -it --name golang-$GOVER hellgate75/golang:$DOCKER_VERSION bash
 echo "Logging docker container: golang-$GOVER ..."
 docker logs -f golang-$GOVER
